75ebcfe4d40d11f8c73564a6ab93137362b0ccaf
anisetiswai <anisetiswai9@gmail.com>
2025-07-20 01:10:13 +0300

    Jukwaa La Ubashiri kwa Kiswahili

---
+++ page.js
@@ -0,0 +1,8 @@
+export default function HomePage() {
+  return (
+    <main>
+      <h1>Karibu BetGeniusTZ!</h1>
+      <p>Hii ni mwanzo wa App yako ya Kisasa kwa Next.js 14.</p>
+    </main>
+  );
+}
