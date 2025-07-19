111ecf9cadd9fdb7b9c7fb5637f431ed809333ec
anisetiswai <anisetiswai9@gmail.com>
2025-07-20 01:17:27 +0300

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <main>
-      <h1>Karibu BetGeniusTZ!</h1>
-      <p>Hii ni mwanzo wa App yako ya Kisasa kwa Next.js 14.</p>
-    </main>
-  );
-}
