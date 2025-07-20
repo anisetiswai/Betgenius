b9d67386276bbff43226fcba143ccc3aba7589d6
anisetiswai <anisetiswai9@gmail.com>
2025-07-20 09:02:59 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <div style={{
+      minHeight: '100vh',
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#f0f0f0',
+      fontFamily: 'sans-serif'
+    }}>
+      <h1>Karibu BetGeniusTZ – Huduma za Kubashiri Mechi kwa Usahihi</h1>
+    </div>
+  );
+}
